4a56f8b91fafd273b7ff1dec123077167805a77e
edemdebrah <ededeb4u@gmail.com>
2025-01-11 08:00:53 +0000

    update to the IntroPage

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -8,50 +8,47 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.brown[400],
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //image or icon display of medical service
-            // Image.asset(
-            //   'lib/images/front_image.png',
-            //   // height: 300,
-            // ),
-            Text(
-              'Image or icon of medical service.',
-              style: TextStyle(fontSize: 12, color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            Text(
-              'Get access to essential digital medical services whereever you are.',
-              style: TextStyle(
-                fontSize: 12,
-                color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //image or icon display of medical service
+              Image.asset(
+                'lib/images/intro3.png',
+                height: 250,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            Text(
-              'GHANA POLICE HOSPITAL',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+
+              SizedBox(
+                height: 40,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 60,
-            ),
-            //Add a button
-          ],
+              Text(
+                'GHANA POLICE HOSPITAL',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Get access to essential digital medical services whereever you are.',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 60,
+              ),
+
+              //Add a button
+            ],
+          ),
         ),
       ),
     );
